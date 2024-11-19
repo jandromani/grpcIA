44d0113e147a2bfbfa3a960d8fe0787e6fff7875
Alejandro BGN <jandromani1989@hotmail.com>
2024-11-19 10:31:57 +0100

    Add files via upload

---
+++ shared-libraries/events/SolicitudEvents.java
@@ -0,0 +1,7 @@
+package events;
+
+public class SolicitudEvents {
+
+    public static final String SOLICITUD_APROBADA = "solicitud.aprobada";
+    public static final String SOLICITUD_RECHAZADA = "solicitud.rechazada";
+}
